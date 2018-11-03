8cf1cdfade0e23746571d0b96064b9a91ce23a90
Andrew Chen <ac1540@berkeley.edu>
2018-11-03 14:40:28 -0700

    adding search bar

---
+++ static/js/places.js
@@ -0,0 +1,29 @@
+function initMap() {
+  
+  var input = document.getElementById('pac-input');
+  
+
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+
+  // Bind the map's bounds (viewport) property to the autocomplete object,
+  // so that the autocomplete requests use the current map bounds for the
+  // bounds option in the request.
+
+  // Set the data fields to return when the user selects a place.
+
+  
+      
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  
+
+  
+}
+
+$(document).ready(function () {
+   console.log('hello');
+   google.maps.event.addDomListener(window, 'load', initMap);
+
+});
