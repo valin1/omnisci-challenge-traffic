aba6d383140d66bc2a6762271f05543552f0416b
Vanessa Lin <valin@berkeley.edu>
2018-11-03 01:11:46 -0700

    Added templates for Flask

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import request
+from flask import redirect, render_template, session, abort
 
 app = Flask(__name__)
 @app.route("/")
-def hello():
-	return "Hello World!"
+def index():
+    return "Flask App!"
+
+@app.route("/hello/<string:name>/")
+def hello(name):
+    return render_template(
+        'test.html',name=name)
+
+@app.route("/members")
+def members():
+	return "Members"
 
 if __name__ == "__main__":
 	app.run()
